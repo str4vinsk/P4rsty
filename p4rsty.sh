f70e5e9f36ce3c9f3586846213e5ebb72bf862dd
Vitor Conroy <64869691+str4vinsk@users.noreply.github.com>
2020-05-13 14:09:17 +0000

    Update p4rsty.sh

--- p4rsty.sh
+++ p4rsty.sh
@@ -3,10 +3,15 @@
 # __author__
 # str4vinsk
 
+if [ "$EUID" -ne 0 ];then
+	echo "[!!] Run p4rsty as root! [!!]"
+  	exit
+fi
+
 if [ $# -lt 1 ];then
 echo " "
-echo "                          [!!] ERRO [!!]                                          "
-echo "          Use: p4rsty (Endereço_do_site)                  "
+echo "                          [!!] ERROR [!!]                                          "
+echo "          Use: p4rsty (site_url)                  "
 echo " "
 exit
 fi
@@ -16,17 +21,17 @@ echo "                          [??] PARSING [??]                               
 echo " "
 
 echo "          [!!] Checking for p4rsty directory... [!!]"
-if [ -e /home/p4rsty/ ];then
+if [ -e /usr/share/p4rsty/ ];then
 echo "          [++] The directory exist."
 echo " "
 else
-echo "          [--] The directory does not exist, creating /home/p4rsty"
-mkdir /home/p4rsty
+echo "          [--] The directory does not exist, creating /usr/share/p4rsty"
+mkdir /usr/share/p4rsty
 echo " "
 fi
 # Aqui pegamos o código fonte atravéz do wget.
 # -O-    ---->    Informamos que não queremos salvar o index em lugar nenhum.
 # -q     ---->    Modo quieto (silencioso)
-wget -q -O- $1 | grep 'href' | grep '\.' | cut -d '"' -f2 | egrep 'http://|https://' | cut -d '/' -f3 | sort -u > /home/p4rsty/result_list
-for n in $(cat /home/p4rsty/result_list);do host $n;done | grep 'has address' | sed 's/has address/--------->/'
+wget -q -O- $1 | grep 'href' | grep '\.' | cut -d '"' -f2 | egrep 'http://|https://' | cut -d '/' -f3 | sort -u > /usr/share/p4rsty/result_list
+for n in $(cat /usr/share/p4rsty/result_list);do host $n;done | grep 'has address' | sed 's/has address/--------->/'
 echo " "
